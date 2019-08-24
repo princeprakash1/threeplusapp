95dbd8f9265c4e1d5c00cad494c72736ad879b36
Prince <prince@Princes-MacBook-Air.local>
2019-08-24 10:07:07 +0530

    First Login and register

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Gate;
+//use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+
+    protected $policies = [ 
+        'App\Model' => 'App\Policies\ModelPolicy', 
+    ];
+
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Passport::routes();
+    }
+}
